9b67198cb468b1a815e26b9f2e33539510b5bb45
root <root@DESKTOP-A28TNE4.localdomain>
2019-03-10 13:06:49 +0100

    mapped user input and replaced static content

--- src/App.js
+++ src/App.js
@@ -2,6 +2,70 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      text: '',
+      reply: [
+        {name: 'Pedro', content: 'Hola bro cuando tiempo'},
+        {name: 'Pedro', content: 'hace años que no te veo'},
+        {name: 'Pedro', content: 'espero no verte más tonto XD'},
+      ]
+    };
+  }
+
+  handleKeyPress = (event) => {
+    switch (event.code) {
+      case "Backspace":
+        this.setState(state => ({
+          text: state.text.substring(0, state.text.length - 1)
+        }));
+        break;
+      case "AltLeft": 
+      case "AltRight": 
+      case "ShiftRight": 
+      case "Tab":
+      case "ControlLeft":
+      case "ControlRight":
+      case "Control":
+      case "Quote":
+      case "ShiftLeft": break;
+      case "Enter":
+        if(this.state.text.length > 1) {
+          let tmp = this.state.reply;
+          tmp.push({name: 'Saúl', content: this.state.text});
+          this.setState(() => ({
+            reply: tmp
+          }));
+        }
+        this.setState(() => ({
+          text: ''
+        }));
+        break;
+      default:
+        if(this.state.text.length < 60) {
+          this.setState(state => ({
+            text: state.text + event.key
+          }));
+        }
+    }
+    
+  };
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress, false);
+  }
+
+  createChatReplies() {
+    let replies = [];
+    for(let i = 0; i < this.state.reply.length; i++){
+      replies.push(<li><span className="userReply">&lt; {this.state.reply[i].name} &gt; </span>{this.state.reply[i].content}</li>)
+    }
+
+    return replies;
+  }
+
   render() {
     return (
       <div className="chatContainer">
@@ -14,12 +78,11 @@ class App extends Component {
           <div className="actualChatContainer">
             <div className="chatTextContainer">
               <ul>
-                <li><span className="userReply">&lt; Pedro &gt; </span>Me he dejado la compra en la basura</li>
-                <li><span className="userReply">&lt; Pedro &gt; </span>menuda cabeza que tengo bro</li>
+                {this.createChatReplies()}
               </ul>
             </div>
             <div className="chatInputBox">
-              <span className="text">aaaa</span><span className="caret">a</span>
+              <span className="text">{this.state.text}</span><span className="caret">a</span>
             </div> 
           </div>
         </div>
