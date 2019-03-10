ed9fed5f4f9331a8a7c647cee3d5ff553304265f
root <root@DESKTOP-A28TNE4.localdomain>
2019-03-10 02:45:55 +0100

    base donete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="chatContainer">
+          <div className="chatContactsContainer">
+            <ul>
+              <li>Pedro</li>
+              <li>Manolo</li>
+            </ul>
+          </div>
+          <div className="actualChatContainer">
+            <div className="chatTextContainer">
+              <ul>
+                <li>Me he dejado la compra en la basura</li>
+                <li>menuda cabeza que tengo bro</li>
+              </ul>
+            </div>
+            <div className="chatInputBox">
+              <span className="text"></span><span className="caret">a</span>
+            </div> 
+          </div>
+        </div>
     );
   }
 }
